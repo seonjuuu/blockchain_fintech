0cf6b2f23d5e9456b80866abb78700832feab6df
seonjuuu <mhnaa6469@gmail.com>
2025-06-14 20:38:10 +0900

    toy_cipher

--- toy_cipher.c
+++ toy_cipher.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+
+uint8_t F(uint8_t finput)
+{
+    uint8_t finput1 = finput<<2;
+    finput1 = (finput1 + finput)%(1<<8);
+    uint8_t finput2 = finput<<4;
+    finput2 = (finput1+finput2)%(1<<8);
+
+    return finput2;
+}
