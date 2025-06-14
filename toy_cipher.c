a19c18f8161be6d83bee92bd7bec46ce702162d0
seonjuuu <mhnaa6469@gmail.com>
2025-06-14 20:40:45 +0900

    toy_cipher

--- toy_cipher.c
+++ toy_cipher.c
@@ -35,6 +35,9 @@ uint16_t toy_cipher(uint16_t pt , uint16_t key)
 }
 
 int main() {
+    uint16_t plaintext = 0x5A3C;
+    uint16_t key = 0x7B28;
 
+    printf("%x",toy_cipher(plaintext, key));
 
 }
