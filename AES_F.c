72ffe4cc3b7266b3a2646bd038c3db59bd8fbe41
seonjuuu <mhnaa6469@gmail.com>
2025-06-19 20:37:25 +0900

    AES_F

---
+++ AES_F.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+
+uint8_t Rkey[11][16];  //
+
+#define AES128
+
+#ifdef AES128
+int Nr = 10; //number round
+int Nb = 16; //number byte
+int Nw = 4; //number words
+#endif
