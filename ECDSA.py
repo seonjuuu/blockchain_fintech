634a21d27a8c77c64e4cb6e2ea591fda8dde3344
seonjuuu <mhnaa6469@gmail.com>
2025-06-29 15:21:11 +0900

    is_oncurve

--- ECDSA.py
+++ ECDSA.py
@@ -11,3 +11,11 @@ gy=0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
 # y^2=x^3+ax+b
 a=0
 b=7
+
+def is_oncurve(x,y):
+    t0=(x**3+a*x+b)%p
+    t1 = (y**2)%p
+    if t0==t1:
+        return 0
+    else:
+        return 1
