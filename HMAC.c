16dd9348ecb8b910c306a96f89214071bcce608a
seonjuuu <mhnaa6469@gmail.com>
2025-07-02 21:26:46 +0900

    SHA-2 Function

--- HMAC.c
+++ HMAC.c
@@ -21,3 +21,16 @@ typedef struct  _HMAC_ALG_INFO{
     uint8_t hmac_digest[SHA256_DIGESTLEN];    
 }_HMAC_ALG_INFO;
 typedef _HMAC_ALG_INFO HMAC_ALG_INFO[1];
+
+#define ROTR_ULONG(x, n) ((uint32_t)((x) >> (n)) | (uint32_t)((x) << (32 - (n))))
+#define ROTL_ULONG(x, n) ((uint32_t)((x) << (n)) | (uint32_t)((x) >> (32 - (n))))
+
+//SHA-2 Functions
+#define CH(x,y,z) ((x&y)^((~x)&z))
+#define Maj(x,y,z) ((x&y)^(x&z)^(y&z))
+
+#define S0(x)  (ROTR_ULONG(x,2)^ROTR_ULONG(x,13)^ROTR_ULONG(x,22))
+#define S1(x)  (ROTR_ULONG(x,6)^ROTR_ULONG(x,11)^ROTR_ULONG(x,25))
+
+#define sig0(x) (ROTR_ULONG(x,7)^ROTR_ULONG(x,18)^((x)>>3)) 
+#define sig1(x) (ROTR_ULONG(x,17)^ROTR_ULONG(x,19)^((x)>>10))
