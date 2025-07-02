8c622407e31ee1ede7248d638994767abb85a34b
seonjuuu <mhnaa6469@gmail.com>
2025-07-02 21:25:50 +0900

    _HMAC_ALG_INFO

--- HMAC.c
+++ HMAC.c
@@ -13,3 +13,11 @@ typedef struct _SHA256_CTX {
 	uint32_t ChainVar[8];
 }_SHA256_CTX;
 typedef _SHA256_CTX SHA256_CTX[1];
+
+//HMAC을 위한 구조체
+typedef struct  _HMAC_ALG_INFO{
+    SHA256_CTX ctx;
+    uint8_t Key[SHA256_BLOCKLEN];
+    uint8_t hmac_digest[SHA256_DIGESTLEN];    
+}_HMAC_ALG_INFO;
+typedef _HMAC_ALG_INFO HMAC_ALG_INFO[1];
