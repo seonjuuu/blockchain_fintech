a32e41a7d7fc3fd74beea71905c344267da9b939
seonjuuu <mhnaa6469@gmail.com>
2025-07-07 20:33:06 +0900

    _SHA256_CTX

--- SHA2.c
+++ SHA2.c
@@ -4,3 +4,12 @@
 
 #define SHA256_BLOCKLEN 64
 #define SHA256_DIGESTLEN 32
+
+//C구조체
+typedef struct _SHA256_CTX {
+	uint8_t data[SHA256_BLOCKLEN]; //8bit*64개 = 512bit (one block)
+	uint32_t datalen; //bytelen
+	uint64_t bitlen; //data bitlen
+	uint32_t ChainVar[8];
+}_SHA256_CTX;
+typedef _SHA256_CTX SHA256_CTX[1];
