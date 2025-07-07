57feb39059727c3f21344b34633c40e8c3d6c0ad
seonjuuu <mhnaa6469@gmail.com>
2025-07-07 20:33:35 +0900

    SHA-2 Functions

--- SHA2.c
+++ SHA2.c
@@ -13,3 +13,16 @@ typedef struct _SHA256_CTX {
 	uint32_t ChainVar[8];
 }_SHA256_CTX;
 typedef _SHA256_CTX SHA256_CTX[1];
+
+#define ROTR_ULONG(x, n) ((uint32_t)((x) >> (n)) | (uint32_t)((x) << (32 - (n))))
+#define ROTL_ULONG(x, n) ((uint32_t)((x) << (n)) | (uint32_t)((x) >> (32 - (n))))
+
+//SHA-2 Functions
+#define CH(x,y,z) ((x&y)^((~x)&z))
+#define Maj(x,y,z) ((x&y)^(x&z)^(y&z))
+
+#define S0(x)  (ROTR_ULONG(x,2)^ROTR_ULONG(x,13)^ROTR_ULONG(x,22))
+#define S1(x)  (ROTR_ULONG(x,6)^ROTR_ULONG(x,11)^ROTR_ULONG(x,25))
+
+#define sig0(x) (ROTR_ULONG(x,7)^ROTR_ULONG(x,18)^((x)>>3)) 
+#define sig1(x) (ROTR_ULONG(x,17)^ROTR_ULONG(x,19)^((x)>>10))
