ad1e492b57bb80feced7951e68a56d99266df871
seonjuuu <mhnaa6469@gmail.com>
2025-07-10 20:43:01 +0900

    Bitcoin_Wallet_Keygen

---
+++ Bitcoin_Wallet_Keygen.py
@@ -0,0 +1,14 @@
+import time
+import hashlib
+import binascii
+import random
+
+#Domain parameters
+p=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
+n=0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+gx=0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798 
+gy=0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+
+# y^2=x^3+ax+b
+a=0
+b=7
