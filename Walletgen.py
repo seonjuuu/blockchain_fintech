e610fdbf017a0c26a3a16510b753973a14ba9720
seonjuuu <mhnaa6469@gmail.com>
2025-07-17 20:14:37 +0900

    esdsa_keygen

--- Walletgen.py
+++ Walletgen.py
@@ -164,3 +164,18 @@ def kmul(k, X, Y, Z):
     #        print("bit :  1")
         i= i>>1
     return tx, ty, tz
+
+
+### ECDSA ###
+
+def ecdsa_keygen():
+    # 개인키 선택
+    d = random.randrange(1,n)
+    # 공개키 연산 Q = [d]G
+    X,Y,Z = kmul(d, gx, gy, 1)
+    zinv=mod_inv(Z,p)
+
+    Qx=(X*zinv)%p
+    Qy=(Y*zinv)%p
+    
+    return d, Qx, Qy
