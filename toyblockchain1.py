fe5ce15c6fd02aeaa0d5a197497b09b4f2078430
seonjuuu <mhnaa6469@gmail.com>
2025-07-24 21:06:01 +0900

    sub

--- toyblockchain1.py
+++ toyblockchain1.py
@@ -8,9 +8,3 @@ class calc:
         return self.first+self.second
     def sub(self):
         return self.first-self.second
-    def mul(self):
-        return self.first * self.second
-    def div(self):
-        q=self.first//self.second
-        r=self.first%self.second
-        return q,r
