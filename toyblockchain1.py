99b1b10d3e054c92d200fd39c4dad1bd815ce176
seonjuuu <mhnaa6469@gmail.com>
2025-07-24 21:05:42 +0900

    add

--- toyblockchain1.py
+++ toyblockchain1.py
@@ -2,3 +2,15 @@ class calc:
     def __init__(self,first,second):
         self.first=first
         self.second=second
+
+    #self.first+self.second
+    def add(self):
+        return self.first+self.second
+    def sub(self):
+        return self.first-self.second
+    def mul(self):
+        return self.first * self.second
+    def div(self):
+        q=self.first//self.second
+        r=self.first%self.second
+        return q,r
