58c95da7c0836d56fe7798e3d2b5f1afc647e6c6
seonjuuu <mhnaa6469@gmail.com>
2025-08-29 19:29:36 +0900

    ecdsa_verify

--- bitstack.py
+++ bitstack.py
@@ -241,3 +241,9 @@ def ecdsa_siggen(m, d):
 
 
 def ecdsa_verify(m,r,s,qx,qy):
+    emsg = m.encode()
+    h = hashlib.sha256(emsg).hexdigest()
+    h = int(h,16)
+    sinv = mod_inv(s,n)
+    u1 = (h*sinv)%n
+    u2 = (r*sinv)%n
