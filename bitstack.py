efb80d133c509fe73e5053c08751c8b364466362
seonjuuu <mhnaa6469@gmail.com>
2025-08-31 19:26:48 +0900

    tx_in

--- bitstack.py
+++ bitstack.py
@@ -273,4 +273,8 @@ def form_length(n):
 incnt = 2
 inn0=['7b46bff49c3eeb7b10c37aca0b930e06bae473f99f34e9c9f45451653847ed6b',0]
 inn1=['7b46bff49c3eeb7b10c37aca0b930e06bae473f99f34e9c9f45451653847ed6b',1]
-d
+def tx_in(incnt, *inn):
+    ver='01000000'
+    in_str = dec_to_little_endian_str(incnt,1)
+    head = ver+in_str
+    ret=[]
