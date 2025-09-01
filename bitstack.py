666579b04d30b05f25b0bc0494f69e4af0dc4960
seonjuuu <mhnaa6469@gmail.com>
2025-09-01 20:34:10 +0900

    gen_txid

--- bitstack.py
+++ bitstack.py
@@ -351,3 +351,8 @@ def gen_txid(head, inn, out, add, d, qx, qy):
     script_list=[]
     addlen = len(add)>>1
     addlen = dec_to_little_endian_str(addlen,1)
+    for i in range(0, len(inn)):
+        msg=""
+        for j in range(0, i):
+            msg = msg + inn[j] + '00'+'ffffffff'
+        msg = msg + inn[i] +addlen + add +'ffffffff'
