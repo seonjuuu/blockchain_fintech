dec6b4b6594ae582d18c67b4ae6939f6935c0baa
seonjuuu <mhnaa6469@gmail.com>
2025-09-09 19:51:46 +0900

    dec_to_little_endian_str

--- ECDSA.py
+++ ECDSA.py
@@ -192,6 +192,10 @@ def dec_to_little_endian_str(n,tlen):
     while len(n_str)  != (tlen<<1):
         n_str = '0' + n_str
     ret=''
+    for i in range(len(n_str)-1,-1,-2):
+        ret = ret+n_str[i-1]
+        ret = ret+n_str[i]
+    return ret
 
 
 
