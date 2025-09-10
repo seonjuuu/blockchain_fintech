55319290a5f741e11845510e95c04b549dbf9ef7
seonjuuu <mhnaa6469@gmail.com>
2025-09-10 21:03:26 +0900

    init

--- bitledger.py
+++ bitledger.py
@@ -3,3 +3,8 @@ from ECDSA import *
 
 class transaction:
     def __init__(self, fromAddr,toAddr, amount):
+        self.fromAddr = fromAddr #str
+        self.toAddr = toAddr #str
+        self.amount = amount #int
+        self.data = fromAddr+toAddr+hex(amount)[2:]
+        self.sig =''
